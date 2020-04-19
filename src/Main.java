521ff9c5f7237223a8b4ad29b44b591a1433bbdc
“mayinghan” <ma.yingha@ufl.edu>
2020-04-18 21:37:02 -0400

    Add topdown & bottomup merge sort

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,20 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int[] arr1 = {4, 2, 5, 1, 7, 3, 5, 2};
+        int[] arr2 = {9, 4, 5, 2, 3, 7, 1, 8};
+
+        Sort.mergeSort(arr1);
+        Sort.mergeSort(arr2);
+
+        printArray(arr1);
+        printArray(arr2);
+    }
+
+    static void printArray(int[] arr) {
+        for(int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
 }
---
+++ src/Sort.java
@@ -0,0 +1,52 @@
+public class Sort {
+    public static int[] mergeSort(int[] arr) {
+        //mergeTopDown(arr, 0, arr.length - 1);
+        mergeBottomUp(arr);
+        return arr;
+    }
+
+    private static void mergeTopDown(int[] arr, int l, int r) {
+        if(l >= r) return;
+
+        int mid = l + (r - l) / 2;
+        mergeTopDown(arr, l, mid);
+        mergeTopDown(arr, mid + 1, r);
+        merge(arr, l, mid, r);
+    }
+
+    private static void mergeBottomUp(int[] arr) {
+        int n = arr.length;
+
+        // merge from size of 1,2,4,8
+        for(int size = 1; size <= n; size += size) {
+            for(int i = 0; i + size< n; i += size + size) {
+                // merge arr[i...i+size-1] and arr[i+size...i+2*size-1]
+                merge(arr, i, i + size -1, Math.min(i + size * 2 - 1, n - 1));
+            }
+        }
+    }
+
+    // merge arr[l .. mid] and arr[mid+1 .. r]
+    private static void merge(int[] arr, int l, int mid, int r) {
+        int[] tmp = new int[r -l + 1];
+        for(int i = l; i <= r; i++) {
+            tmp[i - l] = arr[i];
+        }
+        int i = l, j = mid + 1;
+        for(int k = l; k <= r; k++) {
+            if(i > mid) {
+                arr[k] = tmp[j - l];
+                j++;
+            } else if(j > r) {
+                arr[k] = tmp[i - l];
+                i++;
+            } else if(tmp[i - l] < tmp[j - l]) {
+                arr[k] = tmp[i - l];
+                i++;
+            } else {
+                arr[k] = tmp[j - l];
+                j++;
+            }
+        }
+    }
+}
